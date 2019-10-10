df11229c7f4a851531a039327092475118645480
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-09 23:04:35 -0700

    Bump version to 0.3 for python 3.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='mlutils',
-        version='0.2',
+        version='0.3',
         description='Utilities for training and using Machine Learning models.',
         author='Matthew A. Clapp',
         author_email='itsayellow+dev@gmail.com',
