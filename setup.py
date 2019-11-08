1296183aaadc627b6e7a052b2b13cd3a135aa1a2
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-07 19:37:39 -0800

    Add url, reformat via black.

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='mlutils',
-        version='0.3',
-        description='Utilities for training and using Machine Learning models.',
-        author='Matthew A. Clapp',
-        author_email='itsayellow+dev@gmail.com',
-        packages=[
-            'mlutils'
-            ],
-        install_requires=[
-            #'tensorflow',
-            'keras',
-            'numpy',
-            'matplotlib',
-            'tictoc @ git+https://github.com/itsayellow/tictoc@master',
-            ],
-        python_requires='>=3.6',
-        )
+    name="mlutils",
+    version="0.3",
+    description="Utilities for training and using Machine Learning models.",
+    author="Matthew A. Clapp",
+    author_email="itsayellow+dev@gmail.com",
+    url="https://github.com/itsayellow/mlutils",
+    packages=["mlutils"],
+    install_requires=[
+        #'tensorflow',
+        "keras",
+        "numpy",
+        "matplotlib",
+        "tictoc @ git+https://github.com/itsayellow/tictoc@master",
+    ],
+    python_requires=">=3.6",
+)
